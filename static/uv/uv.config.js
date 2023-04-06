93d901b4c9372e22c66598ea60a0618c86ae8010
NineTen910 <eli.iheanacho@oregonsd.net>
2023-04-06 20:38:45 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ static/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/service/',
+    bare: 'http://172.93.151.179:8080/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
